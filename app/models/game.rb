4463ca994f1ae9487be21046bf639a5707dc6ec4
Umang Shukla <Umang1089@gmail.com>
2017-06-07 12:34:54 -0500

    Updated game create page
    Now need to validate that both teams are in same tournament

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ApplicationRecord
   has_many :questions, :dependent => :destroy
   belongs_to :team_a, :class_name => "Team"
   belongs_to :team_b, :class_name => "Team"
-  
+
   has_one :school_as_team_a, :through => :team_a, :source => :school
   has_one :school_as_team_b, :through => :team_b, :source => :school
   has_one :tournament, :through => :team_a, :source => :tournament
@@ -12,4 +12,10 @@ class Game < ApplicationRecord
   validates :team_b_id, :presence => true
   validates :team_a_id, :presence => true
 
+
+  # validates :same_tournament
+  #
+  # def same_tournament
+  #   errors.add(:team_a, "must be in the same tournament") if :team_a.tournament != :team_b.tournament
+  # end
 end
